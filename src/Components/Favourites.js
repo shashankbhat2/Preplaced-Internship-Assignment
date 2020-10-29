c75724bd2dfbba342a3c15c18e345b3126fabf2c
shashank bhat <shashank.bhat2525@gmail.com>
2020-10-29 19:28:55 +0530

    final assignment

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -12,7 +12,7 @@ const Favourites = () => {
           <h2 class="heading mt-4 mb-4">Your {favourites.length > 0 ? 'Favourites': 'Favourite' }</h2>
           <div class="row">
           {favourites.length === 0 && 
-            <h3 class="heading ml-5 mr-5">No favourite integrations yet. <NavLink to="/integrations">Add Favourite</NavLink></h3>
+            <h3 class="heading ml-5 mr-5">No favourite integrations yet. <NavLink to="/integrations">Add Favourite!</NavLink></h3>
           }
           {favourites && favourites.map(favourite => 
           <div class="col-lg-4 col-md-6 mt-4 mb-4">
